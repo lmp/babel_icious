9f87343b7610d78f7fd9eb990aa73dce3ba4e8b9
Richard Duncan <richard.duncan@leadmediapartners.com>
2014-09-25 17:50:11 -0600

    fix has_key? method on string

--- lib/babel_icious/mappers/hash_map.rb
+++ lib/babel_icious/mappers/hash_map.rb
@@ -51,14 +51,12 @@ module Babelicious
 
     def source_element(source, element)
       value_from_source_element(source, element)
-    rescue TypeError => e
+    rescue NoMethodError => e
       # This method deals with `source` being a string by abusing a strange
       # behavior in ruby 1.8.7. Namely that `""[:foo] => nil` and `""["foo"]
       # => nil`. So in ruby 1.8.7 when a string is passed for the source
-      # parameter, the first two checks will yield nil, and the result will be
-      # the empty string. In ruby 1.9.3, however, `""[:foo]` raises a
-      # TypeError.
-      if /can't convert Symbol into Integer/ === e.message
+      # parameter, it will try has_key? on the string raising a NoMethodError
+      if /undefined method.*has_key?/ === e.message
         ""
       else
         raise e
